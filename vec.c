735ca8e2d85e843cb3399b93ed7d5e3ab5d0c566
Owen Shepherd <owen@owen.cafe>
2022-01-30 12:22:49 +0000

    Make functions for vec operations

---
+++ vec.c
@@ -0,0 +1,24 @@
+#include "vec.h"
+
+void __vec_resize(vec_void *vec, size_t cap, size_t elemsize) {
+  vec->data = realloc(vec->data, cap * elemsize);
+  assert(vec->data != NULL);
+  vec->cap = cap;
+}
+
+void __vec_grow(vec_void *vec, size_t cap, size_t elemsize) {
+  if (vec->cap < cap)
+    __vec_resize(vec, cap, elemsize);
+}
+
+void __vec_push(vec_void *vec, void *el, size_t elemsize) {
+  __vec_grow(vec, vec->len + 1, elemsize);
+  memcpy(((char *)vec->data) + elemsize * vec->len, el, elemsize);
+  vec->len++;
+}
+
+void __vec_append(vec_void *vec, void *els, size_t amt, size_t elemsize) {
+  __vec_grow(vec, vec->len + amt, elemsize);
+  memcpy(((char *)vec->data) + elemsize * vec->len, els, amt * elemsize);
+  vec->len += amt;
+}
