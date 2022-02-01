89851651f14b4cb01f28ccff9fa588fb21174bb7
Owen Shepherd <owen@owen.cafe>
2022-02-01 00:10:27 +0000

    Report test failures in xml report, and time taken

--- util.c
+++ util.c
@@ -10,6 +10,29 @@ void *memclone(void *src, size_t bytes) {
   return dest;
 }
 
+int timespec_subtract(struct timespec *result, struct timespec *x,
+                      struct timespec *y) {
+  /* Perform the carry for the later subtraction by updating y. */
+  if (x->tv_nsec < y->tv_nsec) {
+    int nsec = (y->tv_nsec - x->tv_nsec) / 1000000000 + 1;
+    y->tv_nsec -= 1000000000 * nsec;
+    y->tv_sec += nsec;
+  }
+  if (x->tv_nsec - y->tv_nsec > 1000000000) {
+    int nsec = (x->tv_nsec - y->tv_nsec) / 1000000000;
+    y->tv_nsec += 1000000000 * nsec;
+    y->tv_sec -= nsec;
+  }
+
+  /* Compute the time remaining to wait.
+     tv_usec is certainly positive. */
+  result->tv_sec = x->tv_sec - y->tv_sec;
+  result->tv_nsec = x->tv_nsec - y->tv_nsec;
+
+  /* Return 1 if result is negative. */
+  return x->tv_sec < y->tv_sec;
+}
+
 void ss_init(stringstream *ss) {
   ss->stream = open_memstream(&ss->string, &ss->size);
   if (ss->stream != NULL)
--- util.h
+++ util.h
@@ -17,6 +17,7 @@ typedef struct {
 void ss_init(stringstream *ss);
 void ss_finalize(stringstream *ss);
 
+int timespec_subtract(struct timespec *result, struct timespec *x, struct timespec *y);
 void *memclone(void *src, size_t bytes);
 void memset_arbitrary(void *dest, void* el, size_t amt, size_t elsize);
 char *join(size_t str_amt, char **strs, char *sep);
