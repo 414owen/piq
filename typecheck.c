2e8da3db408ec5bb64170fb684f6ea7bb7148fa1
Owen Shepherd <owen@owen.cafe>
2022-02-01 00:32:27 +0000

    wip tc

--- typecheck.c
+++ typecheck.c
@@ -1,27 +1,30 @@
 #include "typecheck.h"
 
-typedef struct {
-  tc_res res;
-  parse_tree tree;
-} tc_state;
-
-void tc_node(tc_state *s, NODE_IND_T ind) {
-  if (s->res.types.data[ind] != T_UNKNOWN)
-    return;
-  parse_node n = s->tree.nodes.data[ind];
-  switch (n.type) {
-    case PT_CALL: {
-      // tc_node();
-    }
-  }
-}
+typedef enum {
+  POP_VAR,
+  TC_NODE,
+} tc_action;
 
 tc_res typecheck(parse_tree tree) {
-  tc_state state = {
-    .res = {.successful = true, .types = VEC_NEW, .errors = VEC_NEW},
-    .tree = tree,
-  };
-  VEC_REPLICATE(&state.res.types, T_UNKNOWN, tree.nodes.len);
-  tc_node(&state, tree.root_ind);
-  return state.res;
+  tc_res res = {.successful = true, .types = VEC_NEW, .errors = VEC_NEW};
+  vec_node_ind stack = VEC_NEW;
+  VEC_PUSH(&stack, tree.root_ind);
+  VEC_REPLICATE(&res.types, T_UNKNOWN, tree.nodes.len);
+
+  while (stack.len > 0) {
+    NODE_IND_T ind = VEC_POP(&stack);
+    parse_node node = tree.nodes.data[ind];
+    switch (node.type) {
+      case PT_UPPER_NAME: {
+      }
+      case PT_CALL: {
+
+        for (size_t i = 0; i < node.sub_amt; i++) {
+          VEC_PUSH(&stack, tree.inds.data[node.subs_start + i]);
+        }
+      }
+    }
+  }
+
+  return res;
 }
