bc5ae2a173f9349ae1919444f8e38b04ba9a7542
Owen Shepherd <owen@owen.cafe>
2022-02-01 17:30:56 +0000

    Test memclone

--- test_utils.c
+++ test_utils.c
@@ -3,6 +3,18 @@
 
 static char *sep = " . ";
 
+void test_memclone(test_state *state) {
+  test_start(state, "memclone");
+  static int arr[] = {1, 2, 3, 4, 5};
+  int *res = memclone(arr, STATIC_LEN(arr) * sizeof(arr[0]));
+  for (int i = 0; i < STATIC_LEN(arr); i++) {
+    if (res[i] != arr[i])
+      failf(state, "%d != %d", res[i], arr[i]);
+  }
+  free(res);
+  test_end(state);
+}
+
 void test_join(test_state *state, char **strs, size_t amt, char *exp) {
   char *join_res = join(amt, strs, sep);
   if (strcmp(join_res, exp) != 0)
@@ -42,5 +54,6 @@ void run_join_tests(test_state *state) {
 void test_utils(test_state *state) {
   test_group_start(state, "Utils");
   run_join_tests(state);
+  test_memclone(state);
   test_group_end(state);
 }
