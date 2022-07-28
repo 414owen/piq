7c771be59aa1c69614835020e4bcd33e5e86b4d1
Owen Shepherd <owen@owen.cafe>
2022-07-28 19:11:37 +0100

    new env

--- shell.nix
+++ shell.nix
@@ -1,22 +1,20 @@
 { pkgs ? import <nixpkgs> { overlays = [ (import ./nix/overlays.nix) ]; } }:
 
-let 
-  unstable = import <unstable> {};
-in
-
 # pkgs.mkShell.override { stdenv = pkgs.pkgsCross.musl64.stdenv; } {
 pkgs.mkShell {
   nativeBuildInputs = with pkgs; [
-    gcc11
-    unstable.cgdb
+    cgdb
     re2c
     clang-tools
+    clang-tools.clang
     lcov
     lemon
     valgrind
   ];
+
   buildInputs = with pkgs; [
-    llvmPackages_13.libllvm
-    readline
+    # llvmPackages_13.libllvm
+  #   readline
   ];
+
 }
