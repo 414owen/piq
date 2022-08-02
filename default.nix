2c0a1fb362bce0db576571fa806eba2b475b740b
Owen Shepherd <owen@owen.cafe>
2022-08-02 14:18:14 +0100

    Use hedley through nix/niv

--- default.nix
+++ default.nix
@@ -1,7 +1,21 @@
 { pkgs ? import <nixpkgs> { overlays = [ (import ./nix/overlays.nix) ]; } }:
 
+let
+  sources = import ./nix/sources.nix;
+  hedley = pkgs.stdenv.mkDerivation {
+    name = "hedley";
+    version = "v15";
+    src = sources.hedley;
+    dontBuild = true;
+    installPhase = ''
+      mkdir -p $out/include
+      mv hedley.h $out/include
+    '';
+  };
+in
+
 # pkgs.mkShell.override { stdenv = pkgs.pkgsCross.musl64.stdenv; } {
-pkgs.stdenv.mkDerivation {
+pkgs.stdenv.mkDerivation rec {
   name = "lang-c";
 
   src = pkgs.nix-gitignore.gitignoreSource [] ./.;
@@ -22,7 +36,11 @@ pkgs.stdenv.mkDerivation {
   buildInputs = with pkgs; [
     llvmPackages_13.libllvm
     readline81
+    hedley
   ];
+
+  C_INCLUDE_PATH = pkgs.lib.makeSearchPathOutput "dev" "include" buildInputs;
+  CPLUS_INCLUDE_PATH = C_INCLUDE_PATH;
 
   installPhase = ''
     mkdir -p $out/bin
