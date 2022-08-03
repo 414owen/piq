abcdc873c775ea499b43994dbdc6e844a30126c7
Owen Shepherd <owen@owen.cafe>
2022-08-03 19:59:53 +0100

    Fix repl memory leaks

--- src/repl.c
+++ src/repl.c
@@ -49,7 +49,7 @@ end_a:
 
 int main(void) {
   puts("Entering multiline REPL. Press <enter> twice to evaluate.");
-  const char *hist_file_path = join_two_paths(get_cache_dir(), "repl_history");
+  char *hist_file_path = join_two_paths(get_cache_dir(), "repl_history");
   fclose(fopen(hist_file_path, "a"));
   read_history(hist_file_path);
   vec_char multiline_input = VEC_NEW;
@@ -77,4 +77,6 @@ int main(void) {
       free(input);
     }
   }
+  VEC_FREE(&multiline_input);
+  free(hist_file_path);
 }
