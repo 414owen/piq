a527c8a02b5dbef36464b641fdd61e2a4c2cfa71
Owen Shepherd <owen@owen.cafe>
2022-08-20 15:42:55 +0100

    wip

---
+++ src/lower.c
@@ -0,0 +1,18 @@
+#include "ir.h"
+#include "parse_tree.h"
+
+typedef struct {
+  enum { LOWER_NODE } tag;
+  node_ind ind;
+} action;
+
+VEC_DECL(action);
+
+typedef struct {
+  vec_action actions;
+} state;
+
+ir_module lower(parse_tree tree) {
+  ir_module res;
+  
+}
