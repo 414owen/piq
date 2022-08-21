d46e1318736a0b5ac94b6224e8e5a696dcd8bfe5
Owen Shepherd <owen@owen.cafe>
2022-08-21 11:48:16 +0100

    Supress warnings for generated parser

--- scripts/gen-parser.sh
+++ scripts/gen-parser.sh
@@ -5,4 +5,20 @@ set -e
 lemon -c src/parser.y -q
 sed -i 's/^static \(const char \*.*yyTokenName\[\].*\)$/\1/g' src/parser.c
 sed -i 's/assert(/debug_assert(/g' src/parser.c
+tmp="$(mktemp)"
+mv src/parser.c "$tmp"
+echo "#include <hedley.h>" > src/parser.c
+echo "HEDLEY_DIAGNOSTIC_PUSH" >> src/parser.c
+echo '#pragma GCC diagnostic ignored "-Wunused-variable"' >> src/parser.c
+echo '#pragma GCC diagnostic ignored "-Wunused-parameter"' >> src/parser.c
+
+LINES_ADDED=4
+# Add to every self-reference line
+cat "$tmp" \
+  | awk "{if (/#line [0-9]+ \"src\/parser.c\"/) {print \$1,(\$2 + ${LINES_ADDED}),\$3} else {print \$0}}" \
+  >> src/parser.c
+
+echo "HEDLEY_DIAGNOSTIC_POP" >> src/parser.c
+rm $tmp
+
 /bin/sh $(dirname "$0")/add-pragmas.sh src/parser.h
