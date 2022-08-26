bfc57aa0c142eae842cf4d7fddaa0ca6274098ba
Owen Shepherd <owen.shepherd@tiko.energy>
2022-08-26 10:50:15 +0100

    wip

--- src/ir.h
+++ src/ir.h
@@ -7,17 +7,17 @@
 typedef struct {
   binding type_constructor_name;
   node_ind_t data_constructors_start;
-} ir_data_decl;
+} data_decl;
 
-VEC_DECL(ir_data_decl);
+VEC_DECL(data_decl);
 
 typedef struct {
   binding name;
   node_ind_t fields_start;
   node_ind_t field_amt;
-} ir_data_constructor;
+} data_constructor;
 
-VEC_DECL(ir_data_constructor);
+VEC_DECL(data_constructor);
 
 typedef struct {
   enum { TYPE_PARAM, TYPE } tag;
@@ -25,9 +25,9 @@ typedef struct {
     node_ind_t type_param_ind;
     node_ind_t type_module_ind;
   };
-} ir_type_ref;
+} type_ref;
 
-VEC_DECL(ir_type_ref);
+VEC_DECL(type_ref);
 
 typedef enum {
   R_FUN,
@@ -37,10 +37,10 @@ typedef enum {
 VEC_DECL(bnd_type);
 
 typedef struct {
-  vec_ir_data_decl ir_data_decls;
-  vec_ir_data_constructor ir_data_constructors;
-  vec_ir_type_ref ir_type_refs;
-  vec_str_ref ir_refs;
-  vec_bnd_type ir_bnd_types;
+  vec_data_decl data_decls;
+  vec_data_constructor data_constructors;
+  vec_type_ref type_refs;
+  vec_str_ref refs;
+  vec_bnd_type bnd_types;
   bitset ref_is_builtin;
-} ir_module;
+} scope;
--- src/lower.c
+++ src/lower.c
@@ -3,8 +3,10 @@
 #include "vec.h"
 #include "parse_tree.h"
 
+VEC_DECL(scope);
+
 typedef struct {
-  enum { LOWER_NODE } tag;
+  enum { LOWER_NODE, LOWER_TYPE } tag;
   node_ind_t ind;
 } action;
 
@@ -13,7 +15,7 @@ VEC_DECL(action);
 typedef struct {
   vec_action actions;
   parse_tree tree;
-  ir_module res;
+  scope res;
 } state;
 
 static void push_lower_node(state *state, node_ind_t node_ind) {
@@ -21,32 +23,69 @@ static void push_lower_node(state *state, node_ind_t node_ind) {
     .tag = LOWER_NODE,
     .ind = node_ind,
   };
+  VEC_PUSH(&state->actions, a);
 }
 
-static ir_module ir_module_new(void) {
-  ir_module module = {
-    .ir_data_decls = VEC_NEW,
-    .ir_data_constructors = VEC_NEW,
-    .ir_type_refs = VEC_NEW,
-    .ir_refs = VEC_NEW,
+static void push_resolve_type(state *state, node_ind_t node_ind) {
+  action a = {
+    .tag = LOWER_TYPE,
+    .ind = node_ind,
+  };
+  VEC_PUSH(&state->actions, a);
+}
+
+static scope scope_new(void) {
+  scope module = {
+    .data_decls = VEC_NEW,
+    .data_constructors = VEC_NEW,
+    .type_refs = VEC_NEW,
+    .refs = VEC_NEW,
   };
   return module;
 }
 
 static void push_ref(state *state, binding b, bnd_type type) {
   str_ref ref = { .span = b, };
-  VEC_PUSH(&state->res.ir_refs, ref);
-  VEC_PUSH(&state->res.ir_bnd_types, type);
+  VEC_PUSH(&state->res.refs, ref);
+  VEC_PUSH(&state->res.bnd_types, type);
   bs_push(&state->res.ref_is_builtin, false);
 }
 
 static void lower_letrec(state *state, parse_node node) {
+
+
+#define NOT_LETREC \
+  case PT_ROOT: \
+  case PT_CALL: \
+  case PT_CONSTRUCTION: \
+  case PT_FN: \
+  case PT_FN_TYPE: \
+  case PT_FUN_BODY: \
+  case PT_IF: \
+  case PT_INT: \
+  case PT_LIST: \
+  case PT_LIST_TYPE: \
+  case PT_LOWER_NAME: \
+  case PT_STRING: \
+  case PT_TUP: \
+  case PT_AS: \
+  case PT_UNIT: \
+  case PT_UPPER_NAME: \
+  case PT_LET
+
+  bool prev_sig = false;
+  binding prev_bnd;
+
   for (node_ind_t i = 0; i < node.sub_amt; i++) {
     node_ind_t sub_ind = state->tree.inds[node.subs_start + i];
     parse_node sub = state->tree.nodes[sub_ind];
     switch (sub.type) {
       case PT_SIG: {
-        
+        prev_sig = true;
+        node_ind_t bnd_ind = PT_SIG_BINDING_IND(node);
+        prev_bnd = state->tree.nodes[bnd_ind].span;
+        push_resolve_type(state, PT_SIG_TYPE_IND(node));
+        break;
       }
       case PT_FUN: {
         node_ind_t bnd_ind = PT_FUN_BINDING_IND(state->tree.inds, sub);
@@ -54,37 +93,35 @@ static void lower_letrec(state *state, parse_node node) {
         push_ref(state, bnd.span, R_FUN);
         break;
       }
-      case PT_CALL:
-      case PT_CONSTRUCTION:
-      case PT_FN:
-      case PT_FN_TYPE:
-      case PT_FUN_BODY:
-      case PT_IF:
-      case PT_INT:
-      case PT_LIST:
-      case PT_LIST_TYPE:
-      case PT_LOWER_NAME:
-      case PT_STRING:
-      case PT_TUP:
-      case PT_AS:
-      case PT_UNIT:
-      case PT_UPPER_NAME:
-      case PT_LET:
-        UNIMPLEMENTED("findinf ")
-        continue;
+      NOT_LETREC:
+        UNIMPLEMENTED("letrec");
+        break;
     }
   }
   for (node_ind_t i = 0; i < node.sub_amt; i++) {
-    node_ind_t sub = state->tree.inds[node.subs_start + i];
+    node_ind_t sub_ind = state->tree.inds[node.subs_start + i];
+    parse_node sub = state->tree.nodes[sub_ind];
+    switch (sub.type) {
+      case PT_SIG: {
+        break;
+      }
+      case PT_FUN: {
+        break;
+      }
+      NOT_LETREC:
+        UNIMPLEMENTED("letrec");
+        break;
+    }
   }
+#undef NOT_LETREC
 }
 
-ir_module lower(parse_tree tree) {
-  ir_module res;
+scope lower(parse_tree tree) {
+  scope res;
   state state = {
     .actions = VEC_NEW,
     .tree = tree,
-    .res = ir_module_new(),
+    .res = scope_new(),
   };
 
   while (state.actions.len > 0) {
