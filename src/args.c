dc88f1b56bece809d0df6a38bb9ffc6c46940802
Owen Shepherd <owen@owen.cafe>
2022-10-03 23:00:21 +0100

    wip

--- src/args.c
+++ src/args.c
@@ -2,99 +2,173 @@
 #include <stdbool.h>
 #include <string.h>
 #include <stdio.h>
+#include <hedley.h>
 
 #include "args.h"
 #include "bitset.h"
+#include "util.h"
+#include "vec.h"
 
 typedef struct {
   const char **restrict argv;
   const argument *restrict args;
-  size_t arg_cursor;
+  int arg_cursor;
   int argc;
   int argument_amt;
 } parse_state;
-
-typedef struct {
-  char *restrict arg_str;
-  size_t arg_str_len;
-} arg_info;
 
 static bool valid_short_flag(char c) {
   return c >= 'a' && c <= 'z';
 }
 
 void print_help(parse_state *state) {
+  int max_len = 0;
+  for (int i = 0; i < state->argument_amt; i++) {
+    argument a = state->args[i];
+    max_len = MAX(max_len, a.long_len);
+  }
+  for (int i = 0; i < state->argument_amt; i++) {
+    argument a = state->args[i];
+    printf("  -%c      %*s--%s", a.short_name, max_len - a.long_len, "", a.long_name);
+    switch (a.tag) {
+      case ARG_FLAG:
+        putc('\n', stdout);
+        break;
+      case ARG_STRING:
+        puts("   STRING");
+        break;
+      case ARG_INT:
+        puts("   INT");
+        break;
+    }
+  }
 }
 
-static void unknown_arg(parse_state *restrict state, char *arg_str) {
+HEDLEY_NO_RETURN
+static void unknown_arg(parse_state *restrict state, const char *restrict arg_str) {
   fprintf(stderr, "Unknown argument: %s\n", arg_str);
   print_help(state);
   exit(1);
 }
 
-static void parse_long(parse_state *restrict state, arg_info *restrict info) {
+HEDLEY_NO_RETURN
+static void expected_argument(parse_state *restrict state, const char *restrict arg_name) {
+  fprintf(stderr, "Argument %s expected a value, but none given", arg_name);
+  print_help(state);
+  exit(1);
+}
+
+static void parse_long(parse_state *restrict state, const char *restrict arg_str, int arg_str_len) {
+  if (strcmp(arg_str, "help") == 0) {
+    print_help(state);
+    exit(0);
+  }
   for (int i = 0; i < state->argument_amt; i++) {
     argument a = state->args[i];
     // TODO --arg=val form
-    bool matches = strcmp(info->arg_str, a.long_name) == 0;
-    if (matches) {
-      switch (a.tag) {
-        case ARG_FLAG:
+    bool prefix_matches = arg_str_len >= a.long_len && strncmp(arg_str, a.long_name, a.long_len) == 0;
+    bool matches = prefix_matches && arg_str_len == a.long_len;
+    bool matched = false;
+    switch (a.tag) {
+      case ARG_FLAG:
+        if (matches) {
           *a.flag_data = true;
-          break;
-        case ARG_STRING:
-          state->arg_cursor++;
+          matched = true;
+        }
+        break;
+      case ARG_INT:
+      case ARG_STRING:
+        if (matches) {
           if (state->arg_cursor >= state->argc) {
-            fprintf(stderr, "Argument %s expected an argument, but none given.", a.long_name);
-            print_help(state);
-            exit(1);
+            expected_argument(state, a.long_name);
           }
+          state->arg_cursor++;
           *a.string_data = state->argv[state->arg_cursor];
-          break;
-      }
+          matched = true;
+        } else if (prefix_matches && arg_str[a.long_len] == '=') {
+          *a.string_data = state->argv[state->arg_cursor];
+          matched = true;
+        }
+        if (a.tag == ARG_INT) {
+          const char *str = *a.string_data;
+          *a.int_data = atoi(str);;
+        }
+        break;
+    }
+    if (matched) {
       state->arg_cursor++;
       return;
     }
   }
-  unknown_arg(state, info->arg_str);
+  unknown_arg(state, arg_str);
 }
 
-void parse_short(parse_state *state, arg_info *info) {
-  bitset short_flags_covered = bs_new();
-  // "-"
-  if (info->arg_str_len == 0) {
+void parse_short(parse_state *state, const char *restrict arg_str, size_t arg_str_len) {
+  if (strchr(arg_str, 'h')) {
+    print_help(state);
+    exit(0);
+  }
+
+  // just ignore "-"
+  if (arg_str_len == 0) {
     return;
   }
-  for (size_t i = 0; i < info->arg_str_len; i++) {
-    bs_push(&short_flags_covered, false);
-  }
-  bool end_search = false;
-  for (int i = 0; i < !end_search && state->argument_amt; i++) {
-    argument a = state->args[i];
-    switch (a.tag) {
-      case ARG_FLAG:
-        for (size_t i = 0; i < info->arg_str_len; i++) {
-          char c = info->arg_str[i];
-          if (a.short_name == c) {
+  vec_char unmatched = VEC_NEW;
+  for (size_t i = 0; i < arg_str_len; i++) {
+    char c = arg_str[i];
+    bool matched = false;
+    for (int i = 0; i < !matched && state->argument_amt; i++) {
+      argument a = state->args[i];
+      matched |= c == a.short_name;
+      switch (a.tag) {
+        case ARG_FLAG:
+          if (matched) {
             *a.flag_data = true;
-            bs_set(short_flags_covered, i, true);
           }
-        }
-        state->arg_cursor++;
-        break;
-      case ARG_STRING:
-        if (arg_str_len > 1 && strchr(arg_str, a.short_name)) {
-          fprintf(stderr, "Short argument '%c' takes a parameter, so can't be used with other short params.", a.short_name);
-          exit(1);
-        }
-        if (c == )
-        break;
+          break;
+        case ARG_INT:
+        case ARG_STRING:
+          if (matched) {
+            if (arg_str_len > 1) {
+              fprintf(stderr, "Short argument '%c' takes a parameter, so can't be used with other short params.", a.short_name);
+              exit(1);
+            }
+            if (state->arg_cursor >= state->argc) {
+              char s[2] = { a.short_name, '0' };
+              expected_argument(state, s);
+            }
+            state->arg_cursor++;
+            *a.string_data = state->argv[state->arg_cursor];
+          }
+          if (a.tag == ARG_INT) {
+            const char *str = *a.string_data;
+            *a.int_data = atoi(str);;
+          }
+          break;
+      }
+    }
+    if (!matched) {
+      VEC_PUSH(&unmatched, c);
     }
   }
-  unknown_arg(state, info->arg_str);
+  if (unmatched.len > 0) {
+    fputs("Unknown short arguments: ", stderr);
+    fprintf(stderr, "%c", VEC_GET(unmatched, 0));
+    for (size_t i = 1; i < unmatched.len; i++) {
+      fprintf(stderr, ", %c", VEC_GET(unmatched, i));
+    }
+    exit(1);
+  }
+  state->arg_cursor++;
 }
 
-void parse_args(const argument *restrict args, int argument_amt, int argc, const char **restrict argv) {
+void parse_args(argument *restrict args, int argument_amt, int argc, const char **restrict argv) {
+
+  // preprocess
+  for (int i = 0; i < argument_amt; i++) {
+    args[i].long_len = strlen(args[i].long_name);
+  }
+
   parse_state state = {
     .arg_cursor = 1,
     .args = args,
@@ -109,8 +183,6 @@ void parse_args(const argument *restrict args, int argument_amt, int argc, const
     assert(a.short_name == 0 || valid_short_flag(a.short_name));
   }
 
-  bitset short_flags_covered = bs_new();
-
   while (state.arg_cursor < argc) {
     const char *arg_str = argv[state.arg_cursor];
     bool is_long = false;
@@ -123,51 +195,13 @@ void parse_args(const argument *restrict args, int argument_amt, int argc, const
       }
     } else {
       printf("Malformed argument, try adding prefixed dashes: %s\n", argv[state.arg_cursor]);
-      print_help(args, argument_amt);
+      print_help(&state);
     }
 
-    size_t short_flags_covered = 0;
     if (is_long) {
-      parse_long(&state, arg_str);
-    }
-
-    bool end_search = false;
-    for (int i = 0; i < !end_search && argument_amt; i++) {
-      argument a = args[i];
-      switch (a.tag) {
-        case ARG_FLAG:
-          if (is_long) {
-            if (strcmp(arg_str, a.long_name) == 0) {
-              *a.flag_data = true;
-              end_search = true;
-            }
-          } else {
-            for (size_t i = 0; i < arg_str_len; i++) {
-              char c = arg_str[i];
-              if (a.short_name == c) {
-                *a.flag_data = true;
-                short_flags_covered++;
-                if (short_flags_covered == arg_str_len) {
-                  end_search = true;
-                }
-                bs_set(short_flags_covered, i, true);
-              }
-            }
-          }
-          arg_cursor++;
-          break;
-        case ARG_STRING:
-          if (!is_long) {
-            if (arg_str_len > 1 && strchr(arg_str, a.short_name)) {
-              fprintf(stderr, "Short argument '%c' takes a parameter, so can't be used with other short params.", a.short_name);
-              exit(1);
-            }
-            *a.flag_data = true;
-          }
-          arg_cursor++;
-          break;
-      }
+      parse_long(&state, arg_str, strlen(arg_str));
+    } else {
+      parse_short(&state, arg_str, strlen(arg_str));
     }
   }
-  bs_free(&short_flags_covered);
 }
