00a0a391c2c4902de96f82cbe5657a94d19158c0
Owen Shepherd <owen@owen.cafe>
2022-11-16 02:42:27 +0000

    Fix parsing let statements

--- src/test_parser.c
+++ src/test_parser.c
@@ -506,7 +506,7 @@ static void test_parser_succeeds_statements(test_state *state) {
     test_start(state, "Let");
 
     expected_output out = {.tag = STRING,
-                           .str = "(Fun (Lname a) () (Body (Let (Lname b)) ()))"};
+                           .str = "(Fun (Lname a) () (Body (Let (Lname b) ()) ()))"};
     test_parser_succeeds_on(state, "(fun a () (let b ()) ())", out);
 
     test_end(state);
