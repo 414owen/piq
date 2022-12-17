011e614bde8358e5d0a2a950866d6cd0947d4904
Owen Shepherd <owen@owen.cafe>
2022-12-17 17:07:58 +0100

    chore: Remove more things in clean.sh

--- scripts/clean.sh
+++ scripts/clean.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
 
-rm -f *.o repl test src/parser.c src/parser.h src/tokenizer.c main
+rm -f *.o repl test src/parser.c src/parser.h src/tokenizer.c main massif* vgcore* debug-typechecker
