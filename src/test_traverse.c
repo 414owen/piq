2fefbf1cc6523825eb345b757eef0540da3a2670
Owen Shepherd <owen@owen.cafe>
2023-02-06 00:37:09 +0100

    traverse tests wip

--- src/test_traverse.c
+++ src/test_traverse.c
@@ -14,15 +14,45 @@ static const char *action_names[] = {
   #undef mk_map
 };
 
-static bool test_elems_match(test_state *state, pt_traversal *traversal, pt_traverse_elem *elems, int amount) {
+typedef struct {
+  scoped_traverse_action action;
+  union {
+    struct {
+      node_ind_t ind;
+      parse_node_type_all type;
+    } node;
+    uint32_t amount;
+  };
+} test_traverse_elem;
+
+static void test_node_elems_match(test_state *state, pt_traverse_elem a, test_traverse_elem b) {
+  test_assert_eq_fmt_a(state, a.data.node_data.node.type.all, b.node.type, "%s", parse_node_strings);
+  test_assert_eq_fmt(state, a.data.node_data.node_index, b.node.ind, "%d");
+}
+
+static bool test_elems_match(test_state *state, pt_traversal *traversal, test_traverse_elem *elems, int amount) {
   for (int i = 0; i < amount; i++) {
-    pt_traverse_elem a = elems[i];
-    pt_traverse_elem b = pt_scoped_traverse_next(traversal);
+    pt_traverse_elem a = pt_scoped_traverse_next(traversal);
+    test_traverse_elem b = elems[i];
     if (b.action == TR_END) {
       failf(state, "Not enough traversal items. Expected %d.", amount);
     }
     if (a.action != b.action) {
       failf(state, "Traversal item mismatch. Expected '%s', got '%s'.", action_name(a.action), action_name(b.action));
+    }
+    switch (a.action) {
+      case TR_PUSH_SCOPE_VAR:
+      case TR_VISIT_IN:
+      case TR_VISIT_OUT:
+      case TR_NEW_BLOCK:
+        test_node_elems_match(state, a, b);
+        break;
+      case TR_POP_TO:
+        break;
+      case TR_END:
+        break;
+      case TR_LINK_SIG:
+        break;
     }
   }
   pt_traverse_elem b = pt_scoped_traverse_next(traversal);
@@ -44,12 +74,10 @@ static const char *input =
   "(sig a (Fn ((i8, i8)) i8))\n"
   "(fun a ((b, c)) (add ((fn () 2) ()) 3))";
     
-static pt_traverse_elem print_mode_elems[] = {
+static test_traverse_elem print_mode_elems[] = {
   {
     .action = TR_VISIT_IN,
-    .data.node_data = {
-      
-    },
+    .node_ind = 
   },
 };
 
