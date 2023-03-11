29a31a5b7551fc525a80620b3d4bb695af5048c6
Owen Shepherd <owen@owen.cafe>
2023-03-11 15:55:18 +0100

    chore: Max out flamegraph samples

--- scripts/flamegraph.sh
+++ scripts/flamegraph.sh
@@ -1,6 +1,11 @@
 #!/usr/bin/env bash
 
-perf record --call-graph dwarf $@ >&2
+if [ $# = 0 ]; then
+  echo "Usage: $0 <binary> <args>"
+  exit 1
+fi
+
+perf record -F 26000 --call-graph dwarf $@ >&2
 stackpath="$(mktemp)"
 perf script | stackcollapse-perf.pl > "${stackpath}"
 flamegraph.pl "${stackpath}" > flamegraph.svg
