1d514dc56cd6475f2507916e937d4a1c3a56d3c5
Owen Shepherd <owen@owen.cafe>
2023-03-12 16:04:54 +0100

    fix: More POSIX compliance tweaks

--- scripts/add-pragmas.sh
+++ scripts/add-pragmas.sh
@@ -2,9 +2,12 @@
 
 . $(dirname $0)/globs.sh
 
+nl='
+'
+
 fixup() {
 	if ! grep 'pragma once' $1; then
-		echo $'#pragma once\n\n'"$(cat $1)" > $1
+		echo "#pragma once${nl}${nl}$(cat $1)" > $1
 	fi
 }
 
