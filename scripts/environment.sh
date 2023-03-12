777ec71d2b2330e9b622d5e6b1c1ae891cf725ed
Owen Shepherd <owen@owen.cafe>
2023-03-12 15:57:18 +0100

    feat: Search for tools in environment.sh

--- scripts/environment.sh
+++ scripts/environment.sh
@@ -1,5 +1,31 @@
 #!/bin/sh
 
+# This is to be run for non-nix builds
+set +x
+
+getBest() {
+  var="$1"
+  old="$(eval "echo \$${var}")"
+  if [ ! -z "${old}" ]; then
+    echo "Already have ${var} : ${old}"
+    return 0
+  fi
+  echo "Finding a value for ${var}"
+  shift
+  for c in $@; do
+    if [ -x "$(command -v $c)" ]; then
+      export "${var}"="${c}"
+      echo "Found ${c}"
+      return 0
+    fi
+  done
+  echo "Error: None of $@ installed"
+  exit 1
+}
+
+getBest CC cc gcc clang
+getBest CXX c++ g++ clang++
+getBest LD mold lld gold ld
 export READLINE_LIBS=`pkg-config --libs "readline"`
 export READLINE_CFLAGS=`pkg-config --cflags "readline"`
 export LLVM_CONFIG_FLAGS=`llvm-config --cxxflags --ldflags --libs core executionengine mcjit engine analysis native bitwriter --system-libs`
