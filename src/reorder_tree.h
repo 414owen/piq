4b59f685215eef3dd0ccfd79629792418789351b
Owen Shepherd <owen@owen.cafe>
2023-04-07 01:50:00 +0200

    feat: Reorder parse tree for better memory access patterns

---
+++ src/reorder_tree.h
@@ -0,0 +1,9 @@
+// Copyright 2023 The piq Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
+
+#pragma once
+
+#include "parse_tree.h"
+
+parse_tree reorder_tree(parse_tree in);
