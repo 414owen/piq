a3204c20599ee2f572163fda672e4809b4042008
Owen Shepherd <owen@owen.cafe>
2023-04-08 13:35:05 +0200

    feat: Make hash functions generic over number of bits

--- src/hashers.h
+++ src/hashers.h
@@ -2,7 +2,9 @@
 
 #include <stdint.h>
 
-typedef uint64_t hash_t;
+#define HASH_BITS 32
+#define HASH_T uint32_t
+typedef HASH_T hash_t;
 
 hash_t hash_newtype(const void *key_p, const void *ctx_p);
 hash_t hash_stored_type(const void *key_p, const void *ctx_p);
