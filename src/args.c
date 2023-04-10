cb22811fc8bc94f836965be1830627215b97ca85
Owen Shepherd <owen@owen.cafe>
2023-04-10 18:50:14 +0200

    chore: Add licence file reference to the top of each file

--- src/args.c
+++ src/args.c
@@ -1,3 +1,7 @@
+// Copyright 2023 The piq Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
+
 #include <assert.h>
 #include <stdbool.h>
 #include <string.h>
