9bd6e4c0cb3a9b18267b8f1c6ecd63bafab17d13
Owen Shepherd <owen@owen.cafe>
2023-05-12 13:01:12 +0200

    fix: Friendlier output for check scripts

--- scripts/checks/formatting.sh
+++ scripts/checks/formatting.sh
@@ -1,9 +1,26 @@
 #!/usr/bin/env bash
 
-set -euxo pipefail
+set -uo pipefail
 shopt -s extglob
 
 MY_PATH=$(dirname "$0")
 source "${MY_PATH}/../globs.sh"
 
-for i in $handwritten_c_files;  do clang-format -n --Werror $i; done
+num_files_checked=0
+num_checks_failed=0
+for i in $handwritten_c_files; do
+  num_files_checked=$((num_files_checked + 1))
+  clang-format -n --Werror $i;
+  if [ $? != 0 ]; then
+    num_checks_failed=$((num_checks_failed + 1))
+  fi
+done
+
+echo "Checked formatting of $num_files_checked files"
+
+if [ $num_checks_failed = 0 ]; then
+  echo "All checks passed"
+else
+  echo "$num_checks_failed/$num_files_checked files weren't formatted"
+  exit 1
+fi
--- scripts/checks/licences.sh
+++ scripts/checks/licences.sh
@@ -15,15 +15,23 @@ LICENCE="// Copyright 2023 The piq Authors. All rights reserved.
 
 LINES=$(wc -l <(echo "$LICENCE"))
 
+num_files_checked=0
+num_checks_failed=0
 for i in $handwritten_c_files $handwritten_h_files; do
+  num_files_checked=$((num_files_checked + 1))
   if ! cmp --silent <(cat $i | head -n 3) <(echo "$LICENCE") > /dev/null; then
     echo "Top of file ${i} doesn't match desired licence."
     echo "Please copy-paste this to the top:"
     echo "${LICENCE}"
-    fail=true
+    num_checks_failed=$((num_checks_failed + 1))
   fi
-done 
+done
 
-if [ "$fail" = "true" ]; then
+echo "Checked $num_files_checked files for licenses"
+
+if [ $num_checks_failed = 0 ]; then
+  echo "All checks passed"
+else
+  echo "$num_checks_failed/$num_files_checked files weren't formatted"
   exit 1
 fi
