ddc01cd2ec2996daaf03b1a1850a35ee08bda46b
Owen Shepherd <owen@owen.cafe>
2023-05-12 12:57:40 +0200

    fix: Only check handwritten files for licenses

--- scripts/checks/licences.sh
+++ scripts/checks/licences.sh
@@ -1,6 +1,10 @@
 #!/usr/bin/env bash
 
 set -euo pipefail
+shopt -s extglob
+
+MY_PATH=$(dirname "$0")
+source "${MY_PATH}/../globs.sh"
 
 fail="false"
 
@@ -11,14 +15,12 @@ LICENCE="// Copyright 2023 The piq Authors. All rights reserved.
 
 LINES=$(wc -l <(echo "$LICENCE"))
 
-for i in src/*.{c,h}; do
+for i in $handwritten_c_files $handwritten_h_files; do
   if ! cmp --silent <(cat $i | head -n 3) <(echo "$LICENCE") > /dev/null; then
     echo "Top of file ${i} doesn't match desired licence."
     echo "Please copy-paste this to the top:"
     echo "${LICENCE}"
     fail=true
-  # else
-  #   echo "$i checks out"
   fi
 done 
 
