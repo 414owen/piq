d402995c6b551b66cd7bcc75ea51fab43393b5de
Owen Shepherd <owen@owen.cafe>
2023-05-12 13:01:12 +0200

    fix: Friendlier output for check scripts

--- scripts/checks/headers-have-pragmas.sh
+++ scripts/checks/headers-have-pragmas.sh
@@ -1,16 +1,24 @@
 #!/usr/bin/env bash
 
-set -euxo pipefail
+set -euo pipefail
 
 fail="false"
 
+num_files_checked=0
+num_checks_failed=0
 for i in src/*.h; do
+  num_files_checked=$((num_files_checked + 1))
   if ! cat $i | grep 'pragma once' > /dev/null; then
     echo "Header file '$i' doesn't have a pragma once."
-    fail=true
+    num_checks_failed=$((num_checks_failed + 1))
   fi
-done 
+done
 
-if [ "$fail" = "true" ]; then
+echo "Checked pragmas of $num_files_checked files"
+
+if [ $num_checks_failed = 0 ]; then
+  echo "All checks passed"
+else
+  echo "$num_checks_failed/$num_files_checked headers didn't have '#pragma once'"
   exit 1
 fi
